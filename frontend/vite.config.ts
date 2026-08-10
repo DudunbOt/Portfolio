b8d5d43430de719a3fec9503b5272d9938d8f2c1
Pineda Prima <pinedaprima12@gmail.com>
2026-08-10 12:54:13 +0700

    update env setting for dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5297',
+        target: 'http://localhost:8787',
         changeOrigin: true,
         secure: false,
       },
